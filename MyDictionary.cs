40f92c02a0619b1b63c921737ad73008698a9fa5
Mihriban-Karakilic <77552829+Mihriban-Karakilic@users.noreply.github.com>
2021-01-23 00:25:49 +0300

    MyDictionary

---
+++ MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿namespace Dictionary
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        TKey[] tempKeys;
+        TValue[] tempValues;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue Value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = Value;
+        }
+        public int Lenght
+        {
+            get { return keys.Length; }
+        }
+        public TKey[] Keys
+        {
+            get { return keys; }
+        }
+        public TValue[] Values
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MyDictionary<int, string> Kullanici = new MyDictionary<int, string>();
+            Kullanici.Add(100, "Mihriban Karakılıç");
+            Kullanici.Add(140, "Asya Mira Karakılıç");
+            Kullanici.Add(250, "Arif Doğan Karakılıç");
+            Kullanici.Add(524, "Simay Karakılıç");
+
+            Console.WriteLine(Kullanici.Lenght);
+
+            Console.WriteLine("---------------------------------------");
+
+            for (int i = 0; i < Kullanici.Lenght; i++)
+            {
+                Console.WriteLine(Kullanici.Keys[i]+" "+Kullanici.Values[i]);
+
+            }
+            Console.Read();
+        }
+    }
+}
