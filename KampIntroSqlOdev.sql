4b71a7fd9c0cd760a66eb069219f5d46a97bcccc
Mihriban-Karakilic <77552829+Mihriban-Karakilic@users.noreply.github.com>
2021-01-28 22:44:00 +0300

    Add files via upload

---
+++ KampIntroSqlOdev.sql
@@ -0,0 +1,6 @@
+﻿SELECT Products.ProductName, Sum("Order Details".UnitPrice*Quantity) AS ProductSales
+FROM Products
+INNER JOIN 
+(Orders INNER JOIN "Order Details" ON Orders.OrderID = "Order Details".OrderID) 
+ON Products.ProductID = "Order Details".ProductID
+GROUP BY  Products.ProductName
