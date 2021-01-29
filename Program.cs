6d1e13971896b2317787bb5963e7efafbbe5fdb3
Mihriban-Karakilic <77552829+Mihriban-Karakilic@users.noreply.github.com>
2021-01-29 15:58:42 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,26 @@
 ﻿using System;
 
-namespace Dictionary
+namespace AsiDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
-            MyDictionary<int, string> Kullanici = new MyDictionary<int, string>();
-            Kullanici.Add(100, "Mihriban Karakılıç");
-            Kullanici.Add(140, "Asya Mira Karakılıç");
-            Kullanici.Add(250, "Arif Doğan Karakılıç");
-            Kullanici.Add(524, "Simay Karakılıç");
+            bool asilama = false;
+            bool bagisiklik = false;
+            bool riskgrubu = false;
+            int asi = 0;
 
-            Console.WriteLine(Kullanici.Lenght);
+           DateTime tarih = DateTime.Now;
+           DateTime tarihSubat = new DateTime(2021, 2, 1);
+           DateTime tarihMart = new DateTime(2021, 3, 1);
+           DateTime onBesMart = new DateTime(2021,3,15);
 
-            Console.WriteLine("---------------------------------------");
-
-            for (int i = 0; i < Kullanici.Lenght; i++)
-            {
-                Console.WriteLine(Kullanici.Keys[i]+" "+Kullanici.Values[i]);
-
-            }
-            Console.Read();
+           if ((tarih <= tarihMart && asi == 5000000 && asilama == true) && (tarih <= onBesMart && bagisiklik == true && riskgrubu == true))
+                Console.WriteLine("Çok belirgin rahatladık :)");
+           else
+                Console.WriteLine("Çok belirgin ...:( ");
+            Console.ReadLine();
         }
     }
 }
