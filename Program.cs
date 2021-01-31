2cc7e2d36f03b690d1cf2bcca9d6d6110c9aeffc
Mihriban-Karakilic <77552829+Mihriban-Karakilic@users.noreply.github.com>
2021-01-31 19:11:47 +0300

    ReCapProject
    ReCapProject

--- Program.cs
+++ Program.cs
@@ -1,26 +1,12 @@
 ﻿using System;
 
-namespace AsiDemo
+namespace ReCapProject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            bool asilama = false;
-            bool bagisiklik = false;
-            bool riskgrubu = false;
-            int asi = 0;
-
-           DateTime tarih = DateTime.Now;
-           DateTime tarihSubat = new DateTime(2021, 2, 1);
-           DateTime tarihMart = new DateTime(2021, 3, 1);
-           DateTime onBesMart = new DateTime(2021,3,15);
-
-           if ((tarih <= tarihMart && asi == 5000000 && asilama == true) && (tarih <= onBesMart && bagisiklik == true && riskgrubu == true))
-                Console.WriteLine("Çok belirgin rahatladık :)");
-           else
-                Console.WriteLine("Çok belirgin ...:( ");
-            Console.ReadLine();
+            Console.WriteLine("Hello World!");
         }
     }
 }
